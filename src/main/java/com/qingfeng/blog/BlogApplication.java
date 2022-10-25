da500704bae93afdda494d571d01c364cd9dadc7
xiejianhao <xie.jianhao@sinovatio.com>
2022-10-25 14:17:09 +0800

    首次提交

---
+++ src/main/java/com/qingfeng/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+package com.qingfeng.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+
+    public static void main(final String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
